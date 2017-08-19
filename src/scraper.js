05f4b041686d022eacffbe7939c36cbc482ada8d
Sen Hao Yang <senhaoyang@Sende-MacBook-Pro.local>
2017-08-19 16:33:17 +0800

    add 推文數

--- src/scraper.js
+++ src/scraper.js
@@ -6,7 +6,7 @@ const titleSelector = '.title a';
 const titleLinkSelector = '.title a@href';
 const authorSelector = '.meta .author';
 const dateSelector = '.meta .date';
-
+const pushContentSelector = '.nrec';
 export default async (
     boardName = 'Gamesale',
     pages = 3,
@@ -22,6 +22,12 @@ export default async (
                             ? value.match(categoryPatten)[1].trim()
                             : '標題格式錯誤';
                     }
+                },
+                pushContent: value => {
+                    if (value) {
+                        return value.trim();
+                    }
+                    return 0;
                 }
             }
         });
@@ -31,6 +37,7 @@ export default async (
                 category: `${titleSelector}|category`,
                 link: titleLinkSelector,
                 athor: authorSelector,
+                push: `${pushContentSelector}|pushContent`,
                 date: dateSelector
             }
         ])
