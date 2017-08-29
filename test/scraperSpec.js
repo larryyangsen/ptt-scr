af8c8c33f3ca197e6b9fe5c906b7449e5cb412fe
Sen Hao Yang <senhaoyang@Sende-MacBook-Pro.local>
2017-08-29 22:50:19 +0800

    use async/await to test

--- test/scraperSpec.js
+++ test/scraperSpec.js
@@ -1,14 +1,10 @@
 import scraper from '../src/scraper';
 import { expect } from 'chai';
 export default () => {
-  describe('has result', () => {
-    it('get gamesale result', () =>
-      new Promise(resolve => {
-        //   const result = await scraper('Gamesale');
-        return scraper('Gamesale', 0).then(result => {
-          expect(result).to.not.have.lengthOf(0);
-          resolve();
+    describe('has result', () => {
+        it('get gamesale result', async () => {
+            const result = await scraper('Gamesale');
+            expect(result).to.not.have.lengthOf(0);
         });
-      }));
-  });
+    });
 };
