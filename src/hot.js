a26e9c6932ee02fb4bea6281d990a7b7eb535a91
Sen Hao Yang <senhaoyang@Sende-MacBook-Pro.local>
2018-01-20 18:26:42 +0800

    add hot board list

---
+++ src/hot.js
@@ -0,0 +1,27 @@
+import cheerio from 'cheerio';
+import request from './request';
+import storeSession from './store-session';
+import get from './getUrlContent';
+
+const url = 'https://www.ptt.cc/bbs/hotboards.html';
+
+export default async () => {
+    const session = await request();
+    const hotHTML = await get(session, url);
+    const hots = [];
+    let $ = cheerio.load(hotHTML);
+    $('.b-ent').each((i, el) => {
+        $ = $.load(el);
+        const name = $('.board-name').text();
+        const title = $('.board-title').text();
+        const count = $('.board-nuser').text();
+        const boardClass = $('.board-class').text();
+        hots.push({
+            name,
+            title,
+            count,
+            class: boardClass
+        });
+    });
+    return hots;
+};
--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import 'babel-polyfill';
 import scraper from './scraper';
 import content from './content';
-export { scraper, content };
+import hot from './hot';
+export { scraper, content, hot };
+
+// (async () => {
+//     console.log(await hot());
+// })();
 // import fs from 'fs';
 // (async () => {
 //     const option = {
 //         pageCounts: 10,
 //         startPage: 0,
 //         categoryPatten: /\[(.+)\]/,
-//         boardName: 'Gamesale',
+//         boardName: 'nba',
 //         isScrapContent: false
 //     };
 //     console.log(new Date());
 //     const list = await scraper(option);
 //     // console.log(JSON.stringify(list, null,npx 4));
-//     console.log(await content(list.items[0]));
+//     console.log(JSON.stringify(await content(list.items[0])))
 
-//     const csv = list.items.reduce(async (pre, cur) => {
+//     const csv = list.items.reduce( (pre, cur) => {
 //         if (cur.category === '公告' || !cur.title) {
 //             return pre;
 //         }
