69048219ee152959307e4d9639571cf32bda8beb
Sen Hao Yang <senhaoyang@Sende-MacBook-Pro.local>
2018-03-28 23:40:26 +0800

    return contents

--- src/user.js
+++ src/user.js
@@ -10,37 +10,62 @@ const site = '+site:ptt.cc';
 const itemElector = 'div.g';
 const linkSelector = 'h3.r a';
 const descSelector = 'div.s span.st';
-const nextSelector = 'td.b > a';
+const nextSelector = 'td.b > a#pnnext';
 const getSearchResultHTML = async url => await getUrlContent(request, url.toString());
+const getSearchUrl = (url, userName) => {
+    url = new URL(url);
+    // const start = url.searchParams.get('start') || 0;
+    // console.log(start);
+    url.searchParams.set('q', `${userName}${site}`);
+    url.searchParams.set('oq', `${userName}${site}`);
+    url.searchParams.set('ie', 'UTF-8');
+    url.searchParams.set('lang', 'tw');
+    url.searchParams.set('tld', 'tw');
+    url.searchParams.set('sourceid', 'chrome');
+    // url.searchParams.set('start', start);
+    url.searchParams.set('num', 100);
+    // console.log(url);
 
-export default async userName => {
-    const googleSearchUrl = new URL(googleSearch);
-    googleSearchUrl.searchParams.set('q', `${userName}${site}`);
-    googleSearchUrl.searchParams.set('oq', `${userName}${site}`);
-    googleSearchUrl.searchParams.set('ie', 'UTF-8');
-    googleSearchUrl.searchParams.set('lang', 'tw');
-    googleSearchUrl.searchParams.set('tld', 'tw');
-    googleSearchUrl.searchParams.set('sourceid', 'chrome');
-    googleSearchUrl.searchParams.set('num', 100);
-    const userGoogleHTML = await getSearchResultHTML(googleSearchUrl);
-    let $ = cheerio.load(userGoogleHTML);
-    const nextLink = new URL(`${google}${$(nextSelector).attr('href')}`);
-    nextLink.searchParams.set('q', `${userName}${site}`);
+    return url;
+};
+const parseHtml = async (html, userName, contents = []) => {
+    let $ = cheerio.load(html);
+    // const next = $(nextSelector).attr('href');
+    // console.log(next);
+    // let nextLink = next && new URL(`${google}${next}`);
+
     $(itemElector).each((i, ele) => {
         $ = $.load(ele);
         let link = '';
         const linkEle = $(linkSelector);
-        const destEle = $(descSelector);
+        const descEle = $(descSelector);
         const linkObj = querystring.parse(linkEle.attr('href'));
-        const linkTitle = linkEle.text();
-        const descText = destEle.text();
+        const title = linkEle.text();
+        const text = descEle.text();
         if (linkObj['/url?q']) {
             link = linkObj['/url?q'];
         }
-        console.log(i);
-        console.log(linkTitle);
-        console.log(link);
-        console.log(descText);
+        // console.log(i);
+        // console.log(title);
+        // console.log(link);
+        // console.log(text);
+        contents.push({
+            title,
+            link,
+            text
+        });
     });
-    console.log(nextLink.toString());
+    // if (nextLink) {
+    //     nextLink = getSearchUrl(nextLink, userName);
+    //     const nextHtml = await getSearchResultHTML(nextLink);
+    //     return await parseHtml(nextHtml, userName, contents);
+    // }
+    return contents;
+};
+export default async userName => {
+    const googleSearchUrl = getSearchUrl(new URL(googleSearch), userName);
+    const userGoogleHTML = await getSearchResultHTML(googleSearchUrl);
+    const contents = await parseHtml(userGoogleHTML, userName);
+    // console.log(contents.length);
+    return contents;
 };
