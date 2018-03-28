d9e90c1f75dc8bd4ebe31f83c6efa68a7330f334
Sen Hao Yang <senhaoyang@Sende-MacBook-Pro.local>
2018-03-28 23:44:55 +0800

    export user

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import 'babel-polyfill';
 import scraper from './scraper';
 import content from './content';
 import hot from './hot';
-export { scraper, content, hot };
+import user from './user';
+export { scraper, content, hot, user };
